8e5098d469cc72908e9771542ad9b6d33a7b4a70
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-11-14 15:20:04 +0700

    upcode

---
+++ BookManagement/Data/Dto/BookPublisherDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BookManagement.Data.Dto
+{
+    public class BookPublisherDto : ValidatableBindableBase
+    {
+        private Guid _bookPublishercode;
+
+        public Guid BookPublishercode
+        {
+            get { return _bookPublishercode; }
+            set
+            {
+                _bookPublishercode = value;
+            }
+        }
+
+        private string _bookPublisherName;
+
+        public string BookPublisherName
+        {
+            get { return _bookPublisherName; }
+            set
+            {
+                _bookPublisherName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public void ValidateBookGenreName()
+        {
+            ClearErrors(nameof(BookPublisherName));
+            if (string.IsNullOrEmpty(BookPublisherName))
+                AddError(nameof(BookPublisherName), "Không được bỏ trống!");
+            if (BookPublisherName?.Length > 256)
+                AddError(nameof(BookPublisherName), "Vượt quá 256 ký tự!");
+        }
+    }
+}
---
+++ BookManagement/Model/BookPublisherModel.cs
@@ -0,0 +1,15 @@
+﻿using BookManagement.Data;
+using BookManagement.Data.Dto;
+using System.Collections.ObjectModel;
+
+namespace BookManagement.Model
+{
+    public class BookPublisherModel : DataModel<BookPublisherDto>
+    {
+        public BookPublisherModel()
+        {
+            Model = new BookPublisherDto();
+            Models = new ObservableCollection<BookPublisherDto>();
+        }
+    }
+}
