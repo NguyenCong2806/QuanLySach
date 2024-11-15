780eed18374ea01bf7900889a956ee53247b0379
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-11-16 06:33:35 +0700

    UPCODE

--- BookManagement/ViewModel/BookViewModel.cs
+++ BookManagement/ViewModel/BookViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using BookManagement.Utilities;
+using BookManagement.View;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BookManagement.ViewModel
 {
     public class BookViewModel
     {
+        public BookViewModel()
+        {
+
+        }
+        private ICommand _nextPageAddAndEditCommand;
+
+        public ICommand NextPageAddAndEditCommand
+        {
+            get
+            {
+                if (_nextPageAddAndEditCommand == null)
+                    _nextPageAddAndEditCommand = new AsyncRelayCommand<object>(param => NextPage(), null);
+
+                return _nextPageAddAndEditCommand;
+            }
+        }
+
+        private async Task NextPage()
+        {
+            BookAddAndEdit bookAddAndEdit = new BookAddAndEdit();
+            bookAddAndEdit.
+            await Task.CompletedTask;
+        }
+        
     }
 }
